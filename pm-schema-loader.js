4fda8d35a9ed0ffdf37f27707dac80c87f21eaef
NadiyaDyka <72070878+NadiyaDyka@users.noreply.github.com>
2021-01-13 16:22:35 +0100

    alfa-ver of schema-loader (load schema from GitHub)

--- pm-schema-loader.js
+++ pm-schema-loader.js
@@ -1,104 +1,189 @@
+const _dummy = setInterval(() => {}, 300000);
 const Ajv = require('ajv');
-/**
+/** 
+ *importent information how Postman SandBox can work with asinc/await
+ * https://community.postman.com/executing-sync-requests-programmatically-from-a-pre-request-or-test-scripts/14678
  * Main idea: download from GitHub JSON-schemas and subschemas
  * (if we haven't got them in sources yet) and when we get all needs schemas,
- * transfer them (add) into library ajv for compilation. 
- * To realize this idea we create js-class that managed schemas downloading.
+ * transfer them (add) into library ajv for compilation, then all information will storege in class 
+ * To realize this idea we created js-class that managed schemas downloading.
  */
 const descr = {
-    "host": "https://github.com/NadiyaDyka/AffRegAPIDoc",
+    "host": "https://api.github.com/repos/NadiyaDyka/AffRegAPIDoc/contents",
     "schemas": {
 
         "get_lang_schema": {
             "title": "Schema for GET language response",
-            "path": "/schemas/get_lang_schema.json",
-            //            "include": ["common_lib", "data_lib"]
-            /*  collection of subschemas in format id: subschema_name */
+            "path": "/schemas/get_lang_schema.json",           
             "include": { main: "common_lib", reserve: "common_lib", glsData: "data_lib" }
         },
 
         "common_lib": {
             "path": "/schemas/common_lib.json",
-            "include": { metaId: "meta_lib", dataID: "data_lib" },
+            "include": { metaId: "meta_lib", dataID: "data_lib" }
         },
-
         "data_lib": {
             "path": "/schemas/data_lib.json"
-
         },
-
         "meta_lib": {
             "path": "/schemas/meta_lib.json"
         }
+
     }
 }
 
-class PMSchemaLoader {
+class PMSchemaLoadManager {
     descr = {};
     host = "";
     /**
      * storege for schema sources
      */
     sources = {};
-    LockFlag = {};
+
     constructor(descr, host) {
         this.descr = descr;
         this.host = host;
     }
 
-    getSchema(schema_name) {
-        let subschemas = this.load(schema_name);
-        console.log(subschemas);
-        this.LockFlag = {};
-        console.log(`getSchema: befor new Ajv`);
+    /**
+     * Function forms a string with the url and
+     * than create object with request parameters 
+     * @param {string} SchemaName Name of Schema
+     * @throws Exception if any error
+     * @returns {object} request parameters for get Schema
+     */
+    getSchemaRequestParameters(SchemaName) {
+            if (!this.descr['schemas'][SchemaName]['path']) {
+                throw new Error(`Can't find property 'path' in descriptor for ${SchemaName}`)
+            };
+            let requestParameters = {};
+            let url = this.host + this.descr['schemas'][SchemaName]['path'];
+            requestParameters.url = url;
+            /**
+             *  the code below (metod and header) need to change to use Postman's constants 
+             *  or change to transfer header as parameter of class?
+             */
+            requestParameters.method = "GET";
+            requestParameters.header = {
+                'Accept': 'application/vnd.github.VERSION.raw',
+                'Authorization': 'token 4ac77666d4a0013f7cb791d0319d412a59653db6'
+            };            
+            return requestParameters;
+        }
+        /**
+         * manages the generating list of schemas to load from host      
+         * @param {string} SchemaName
+         * @throws Exception if any error
+         * @returns {object}
+        /*
+        это функция-генератор. Она не собирает список, а как только нашла нужный элемент
+        передает его дальше в работу. 
+        Клиенту с этой функцией нужно работать так:
+        async function(){
+          try {
+            foreach( var scheme in GSM.getSchemasToLoadFor(SchemaName) ) { // *1
+              GSM.processLoad( await sendRequest( GSM.getSchemaRequestParameters(scheme) ) );
+            }
+            ajv = GSM.getAJV( SchemaName );
+          }
+          catch(...)
+        }();
+        */
+
+    * getSchemasToLoadFor(SchemaName, excludeList = {}) {
+        if (!this.descr['schemas'][SchemaName]) {
+            throw new Error(`Can't find the schema ${SchemaName} in descriptor.`)
+        }
+          //  console.log(`покажи текущий элемент соурс и потом весь ниже`);
+         //   console.log(this.sources[SchemaName]); 
+         //   console.log(this.sources); 
+        if (!this.sources[SchemaName] &&
+            !excludeList[SchemaName]) {              
+            yield SchemaName;
+            excludeList[SchemaName] = true;            
+           
+        }
+
+        if (this.descr['schemas'][SchemaName]['include']) {
+
+            if (typeof(this.descr['schemas'][SchemaName]['include']) == "object") {
+
+                for (let id in this.descr['schemas'][SchemaName]['include']) {
+
+                    let subschema = this.descr['schemas'][SchemaName]['include'][id];
+                    yield* this.getSchemasToLoadFor(subschema, excludeList);
+                }
+            }
+        }
+    }
+
+    /**
+     * creates an instance of the library and passes it a list of subschemas with id 
+     * @param {string} SchemaName
+     * @throws Exception if any error
+     * @returns {object} validate
+     * 
+     */
+    addSchemaToAjv(SchemaName, sources) {
+        let subschemas = this.collectListOfSubschemas(SchemaName);
+       // console.log("ниже будет соурс");       
+      //  console.log(this.sources);
+        // Because Promises in Postman are still buggy (see https://community.postman.com/t/using-native-javascript-promises-in-postman/636/11)
+        // We have no other way as use own waiting function.
+        // this.waitForLoad(SchemaName, subschemas);
+
+       // console.log(`getSchema: befor new Ajv`);
         let ajv = new Ajv({ logger: console, allErrors: true, verbose: true });
-        console.log(`getSchema: after new Ajv`);
+      //  console.log(`getSchema: after new Ajv`);
         for (let key in subschemas) {
-            let subschema_name = subschemas[key];
-            console.log(`Add subschema: ${subschema_name}`);
-            console.log(this.sources[subschema_name]);
-            ajv.addSchema(this.sources[subschema_name], key);
+            let subSchemaName = subschemas[key];
+            console.log(`Add subschema: ${subSchemaName}`);
+            console.log(JSON.parse(this.sources[subSchemaName]));
+            ajv.addSchema(JSON.parse(this.sources[subSchemaName]), key);
+            console.log(`key ${key}`);
+            console.log(JSON.parse(this.sources[SchemaName]));
         }
         try {
-            let validate = ajv.compile(schema_name);
-            console.log("I'm compile something!");
+            
+        const schemaValidator = ajv.compile(JSON.parse(this.sources[SchemaName]));
+            console.log("I compile schema!");
         } catch (e) {
             console.log(e.message);
         }
-        return validate;
+        return schemaValidator;
     }
 
 
     /**
      * manages the loading of the main schema and subschemas, 
      * generates a list of subschemas
-     * @param {string} schema_name 
+     * @param {string} SchemaName
      * @throws Exception if any error
      * @returns {object}
      * 
      */
-    load(schema_name) {
-        if (!this.descr['schemas'][schema_name]) {
-            throw new Error(`Can't find the schema ${schema_name} in descriptor.`)
+    collectListOfSubschemas(SchemaName) {
+        if (!this.descr['schemas'][SchemaName]) {
+            throw new Error(`Can't find the schema ${SchemaName} in descriptor.`)
         }
-        let succses = this.loadItem(schema_name);
-
         var subschemas = {};
-        if (this.descr['schemas'][schema_name]['include']) {
+        if (this.descr['schemas'][SchemaName]['include']) {
 
-            if (typeof(this.descr['schemas'][schema_name]['include']) == "object") {
+            if (typeof(this.descr['schemas'][SchemaName]['include']) == "object") {
 
-                for (let id in this.descr['schemas'][schema_name]['include']) {
+                for (let id in this.descr['schemas'][SchemaName]['include']) {
 
-                    /* Attention! This function returns only subschemas, it mean that "main" schema (schema_name) not
+                    /* Attention! This function returns only subschemas, it mean that "main" schema (SchemaName) not
                     be include to list, so we need add it "manually"*/
 
-                    subschemas[id] = this.descr['schemas'][schema_name]['include'][id];
+                    subschemas[id] = this.descr['schemas'][SchemaName]['include'][id];
 
-                    Object.assign(subschemas, this.load(subschemas[id]));
+                    Object.assign(subschemas, this.collectListOfSubschemas(subschemas[id]));
                 }
             }
         }
+        console.log("subschemas ниже");  
+        console.log(subschemas);
         return subschemas;
     }
 
@@ -107,91 +192,67 @@ class PMSchemaLoader {
      * Analitic function for download schema.
      * It analyzes availability for download and checks the sources for an element with the same name.
      * If there is no element with current name, add the loaded schema to the sources
-     * @param {string} schema_name Name of schema
+     * @param {string} SchemaNameName of schema
      * @throws Exception if any error
      * @returns {boolean} Success flag
      */
-    async loadItem(schema_name) {
+    processLoad(Key, Schema) {
+       // console.log(`Зашли в processLoad и смотрим SchemaJSON`);
+       // console.log(JSON.stringify(pm.response.json()));
+        //console.log(this.sources[Schema]);
+        let SchemaJSON=JSON.stringify(Schema.json());
+        //console.log(Key);
+        if (!this.sources[SchemaJSON]) { 
+            //console.log(`Зашли в иф -значит элемент отсутствует в соурсе`);          
+            //here i need to check that sendRequest return resolve result (not error)
+            // if sendRequest return result than res=this.sources[SchemaName]
 
-        if (!this.sources[schema_name]) {
-            console.log(`loadItem: Before _loadItem ${schema_name}`)
-            if (!this.LockFlag[schema_name]) {
-                this.LockFlag[schema_name] = 1;
-                //here i need to check that this._loadItem return resolve result (not error)
-                // if this._loadItem return result than res=this.sources[schema_name]
-                this.sources[schema_name] = await this._loadItem(this.getSchemaRequestParameters(schema_name));
-                console.log(`loadItem: After _loadItem ${schema_name}`);
-            } else {
-                console.log(`loadItem: schema already locked ${schema_name}`);
-            }
+            //const result = await sendRequest(this.getSchemaRequestParameters(SchemaName));
 
-        } else {
-            console.log(`loadItem: schema already loaded ${schema_name}`);
+            if (pm.expect(Schema).to.have.property('code', 200) &&
+                pm.expect(Schema).to.have.property('status', 'OK')) {
+                this.sources[Key] = SchemaJSON;
+               // console.log(this.sources[Schema]);
+            }            
         }
+        //console.log(`На выходе processLoad`);
+        
         return true;
     }
+}
 
-    /**
-     * Internal Function download schema used Request Parameters
-     * (It doesn't analyzes anything)
-     * @param {string} url Url of schema
-     * @throws Exception if any error
-     * @returns {object} Schema Request Parameters
-     */
-    _loadItem(requestParameters) {
-            /*function sendRequest(req) {*/
-            return new Promise((resolve, reject) => {
-                // console.log("_loadItem");
-                pm.sendRequest(requestParameters, (err, res) => {
-                    if (err) {
-                        return reject(err);
-                    }
+/**
+ * External Function download schema used Request Parameters
+ * (It doesn't analyzes anything)
+ * @param {string} url Url of schema
+ * @throws Exception if any error
+ * @returns {object} Schema Request Parameters
+ */
+function sendRequest(req) {
+    return new Promise((resolve, reject) => {
+        pm.sendRequest(req, (err, res) => {            
+            if (err) {
+                return reject(err);
+            }                   
+            return resolve(res);
 
-                    return resolve(res);
-                })
-            });
+        })
+    });
+}
+
+const sl = new PMSchemaLoadManager(descr, "https://api.github.com/repos/NadiyaDyka/AffRegAPIDoc/contents");
+
+async function wrapper() {
+    try {
+        for (var schema of sl.getSchemasToLoadFor("get_lang_schema")) {  
+          //  console.log(`генератор выдал название схемы ${schema}`);          
+            sl.processLoad(schema, await sendRequest(sl.getSchemaRequestParameters(schema)));
         }
-        //*/
-        // return "Placeholder for " + requestParameters;
-
-
-    /**
-     * Function forms a string with the url and
-     * than create object with request parameters 
-     * @param {string} schema_name Name of Schema
-     * @throws Exception if any error
-     * @returns {object} request parameters for get Schema
-     */
-    getSchemaRequestParameters(schema_name) {
-        if (!this.descr['schemas'][schema_name]['path']) {
-            throw new Error(`Can't find property 'path' in descriptor for ${schema_name}`)
-        };
-        let requestParameters = {};
-        let url = this.host + this.descr['schemas'][schema_name]['path'];
-        requestParameters.url = url;
-        /**
-         *  the code below (metod and header) need to change to use Postman's constants 
-         *  or change to transfer header as parameter of class?
-         */
-
-        requestParameters.method = "GET";
-        requestParameters.header = {
-            'Accept': 'application/vnd.github.VERSION.raw',
-            'Authorization': 'token 4ac77666d4a0013f7cb791d0319d412a59653db6'
-        };
-        // return url;
-        //console.log(requestParameters);
-        return requestParameters;
-
+        ajv = sl.addSchemaToAjv("get_lang_schema");        
+        clearInterval(_dummy)
+    } catch (ex) {
+        console.log(`Fail: name: ${ex.name}; message: ${ex.message}`);
+        clearInterval(_dummy)
     }
 }
-
-const sl = new PMSchemaLoader(descr, "https://github.com/NadiyaDyka/AffRegAPIDoc");
-
-try {
-    let x = sl.getSchema("get_lang_schema");
-    //console.log("See X on next row");
-    console.log(x);
-} catch (ex) {
-    console.log(`Fail: name: ${ex.name}; message: ${ex.message}`);
-}
+wrapper();
